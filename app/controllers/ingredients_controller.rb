0516eba691b58d99da53772308e81ee3d47aa1d2
raulchopi <raul.cacabelos@gmail.com>
2013-10-29 20:42:45 +0100

    Definida la estructura del menu buscador lateral en la pantalla principal y comenzamos a definir dicho buscador de ingredientes y su controlador

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,15 @@
 class IngredientsController < ApplicationController
+
+	def ingredient_by_name
+  		@ingredient = Ingredient.where(:name => params[:name])
+	end
+
+	def show
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def new
+    	@ingredient = Ingredient.new
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907134824) do
+ActiveRecord::Schema.define(version: 20131016164406) do
 
   create_table "authorizations", force: true do |t|
     t.string   "provider"
@@ -21,63 +21,50 @@ ActiveRecord::Schema.define(version: 20130907134824) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "surname"
-    t.string   "username"
+  create_table "ingredients", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "links", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "opinions", force: true do |t|
-    t.integer   "recipe_id"
-    t.integer   "user_id"
-    t.float     "rating"
-    t.text      "opinion"
-    t.datetime  "created_at"
-    t.datetime  "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "receipts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "recipes", force: true do |t|
-    t.integer   "user_id"
-    t.string    "title"
-    t.integer   "difficulty"
-    t.integer   "time"
-    t.integer   "servings"
-    t.text      "description"
-    t.float     "rating"
-    t.datetime  "created_at"
-    t.datetime  "updated_at"    
-  end
-
-  create_table "ingredients", force: true do |t|
-    t.string    "name"
-    t.integer   "importance"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "steps", force: true do |t|
-    t.integer   "recipe_id"
-    t.integer   "order"
-    t.integer   "time"
-    t.text      "description"
-  end
-
-  create_table "likes", force: true do |t|
-    t.integer   "recipe_id"
-    t.integer   "user_id"
-    t.datetime  "created_at"
-  end
-
-  create_table "links", force: true do |t|
-    t.integer   "recipe_id"
-    t.integer   "ingredient_id"
-    t.integer   "unit_id"
-    t.float     "number"
-    
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "units", force: true do |t|
-    t.string    "unit"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
