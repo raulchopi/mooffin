c848a7738f45620c50aa37af7b8098dd5dadda6b
raulchopi <raul.cacabelos@gmail.com>
2013-12-03 09:06:38 +0100

    Comportamento e efectos do boton de login da cabeceira

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-function foo() {
-	document.getElementById('login_nav').style.visibility = 'hidden'; 
-	document.getElementById('login_form').style.visibility = 'true'; 
-}
+
+// ######  HEADER BEHAVIOUR ######
+
+$(document).ready(function(){
+	$("#login_form").hide();
+});
+
+$(".btn_login").click(function(){
+    $("#login_nav").hide();
+	$("#login_form").fadeIn('slow');
+  });
+
+$(".btn_close_login").click(function(){
+    $("#login_form").hide();
+	$("#login_nav").fadeIn('slow');
+  });
