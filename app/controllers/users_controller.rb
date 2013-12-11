11be6f0a6129611f41e56d011fddcea360635bc7
raulchopi <raul.cacabelos@gmail.com>
2013-12-11 18:24:40 +0100

    Formulario de rexistro de novos usuarios funcionando

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,18 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-	def login
-
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to root_url, :notice => "Signed up!"
+		else
+			redirect_to signup_path, :alert => "Error en el registro!"
+		end
 	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:username, :name, :surname, :email, :password)
+		end
 end
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -170,7 +170,7 @@ Rails.application.config.sorcery.configure do |config|
     # change default crypted_password attribute.
     # Default: `:crypted_password`
     #
-    # user.crypted_password_attribute_name =
+    # user.crypted_password_attribute_name = :password
 
 
     # what pattern to use to join the password with the salt
