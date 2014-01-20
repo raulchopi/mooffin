5e10af4312b835029cda704f61b9bc59c9261310
raulchopi <raul.cacabelos@gmail.com>
2014-01-20 19:00:31 +0100

    Mostrar receta concreta, comezos

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,12 @@
 class RecipesController < ApplicationController
+	before_action :get_recipe
+
+	def get_recipe
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def show
+		@links 			= @recipe.links
+		@steps			= @recipe.steps
+	end
 end
