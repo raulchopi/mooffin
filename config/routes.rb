e908e2b14e043d02b0f9aedc44944cc76b7eec85
raulchopi <raul.cacabelos@gmail.com>
2014-01-20 16:32:56 +0100

    Comezo da pantalla das recetas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Mooffin::Application.routes.draw do
   root :to => 'home#index'
 
   get '/signup' 	=> 'users#new', 		:as => 'signup'
+  get '/recetas' 	=> 'recipes#show',		:as => 'recipes'
 
   get 'login'   	=> 'sessions#new',   	:as => 'login'
   get 'logout'  	=> 'sessions#destroy',  :as => 'logout'
