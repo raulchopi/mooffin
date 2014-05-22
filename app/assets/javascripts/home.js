bc295e345b7cfd7266719b1b44277ea292346edd
raulchopi <raul.cacabelos@gmail.com>
2014-05-22 14:02:34 +0200

    mellorados os estilos da cabeceira

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,59 +7,9 @@ $(document).ready(function(){
   //var images = ['portada1.png'];
   //$('#imagenInicio').css({'background': 'url(../images/portada/' + images[Math.floor(Math.random() * images.length)] + ') no-repeat'});
 
-  $("#login_box").hide();
-
-
-
-  // HEADER BEHAVIOUR ON SCROLL
-  $(window).scroll(function () {
-    var y = $(this).scrollTop();
-    var z = $('#menu_principal').offset().top;
-    var header = $('#header');
-    var content = $('#menu_principal');
-
-    if (y >= 1) {
-      header.removeClass('big').addClass('small');
-      content.css('margin-top','50px');
-      marginLogin();
-    }
-    else{
-      header.removeClass('small').addClass('big');
-      content.css('margin-top','90px');
-      marginLogin();
-    }
-  });
-
-  //Aplica margen al cuadro de login en la pantalla para movil
-  function marginLogin(size){
-    if($(this).width() >= 658){
-      $("#login").removeClass('movil');
-    }
-    else if($(this).width() < 658){
-      $("#login").addClass('movil');
-    }
-  }
-
-
 });
 
 
-// ######  HEADER BEHAVIOUR ######
-
-$(".btn_login").click(function(){
-  $("#login_nav").hide();
-  $("#login_form").fadeIn();
-  $("#login_omniauth").fadeIn();
-  $("#login_box").slideDown();
-});
-
-$(".btn_close_login").click(function(){  
-  $("#login_form").fadeOut();
-  $("#login_omniauth").fadeOut();
-	$("#login_box").slideUp();
-  $("#login_nav").delay(500).fadeIn();
-  
-});
 
 
 // ######  SEARCHER BEHAVIOUR ######
--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
+	$("#login_box").hide();
+
 	//CARGA DE PÁGINA EN PANTALLAS PEQUEÑAS
 	if($(this).width() < 658){
 		$(".menu").slideUp();
-		$("#menu_principal").addClass('movil');
-		$("#login").addClass('movil');
 		$(".menu").css("border-bottom", "2px solid black");
 		$(".menu").css("-webkit-box-shadow", "inset 0px 0px 50px 0px rgba(41, 43, 51, 0.25)");	
 	}
@@ -12,6 +12,8 @@ $(document).ready(function(){
 	// VARIABLES
 	var desplegado = false;
 	var ancho = $(window).width();
+	var header = $('#header');
+	var content = $('#menu_principal');
 
 	// Boton para (des)plegar el menu para dispositivos moviles
 	$("#menu_movil").click(function() {
@@ -35,16 +37,14 @@ $(document).ready(function(){
 			desplegado = true;
 			$(".menu").css("border-bottom", "none");
 			$(".menu").css("-webkit-box-shadow", "none");
-			$("#menu_principal").removeClass('movil');
-			$("#login").removeClass('movil');
+			$('#menu_principal').css('margin-top','90px');	//Se fuerza a que coja este margen
 	   }
 	   else if($(this).width() < 658){
 	   		$(".menu").hide();
 			desplegado = false;
 			$(".menu").css("border-bottom", "2px solid black");
 			$(".menu").css("-webkit-box-shadow", "inset 0px 0px 50px 0px rgba(41, 43, 51, 0.25)");
-			$("#menu_principal").addClass('movil');
-			$("#login").addClass('movil');
+			$('#menu_principal').css('margin-top','120px'); //Se fuerza a que coja este margen
 	   }
 	});
 
@@ -73,6 +73,46 @@ $(document).ready(function(){
 				//desplegado = true;	      
 	    	}
 		}
+	});
+
+
+	// HEADER BEHAVIOUR ON SCROLL
+	$(window).scroll(function () {
+		var y = $(this).scrollTop();
+
+		if (y >= 1) {
+			header.removeClass('big').addClass('small');
+			content.css('margin-top','50px');
+		}
+		else{
+			header.removeClass('small').addClass('big');
+			marginLogin();
+		}
+	});
+
+	//Aplica margen al cuadro de login en la pantalla para movil
+	function marginLogin(){
+		if($(this).width() >= 658){
+			content.css('margin-top','90px');
+		}
+		else if($(this).width() < 658){
+			content.css('margin-top','120px');
+		}
+	}
+
+	// ######  HEADER BEHAVIOUR ######
+	$(".btn_login").click(function(){
+		$("#login_nav").hide();
+		$("#login_form").fadeIn();
+		$("#login_omniauth").fadeIn();
+		$("#login_box").slideDown();
+	});
+
+	$(".btn_close_login").click(function(){  
+		$("#login_form").fadeOut();
+		$("#login_omniauth").fadeOut();
+		$("#login_box").slideUp();
+		$("#login_nav").delay(500).fadeIn();
 	});
 
 
