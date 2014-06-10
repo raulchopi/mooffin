ce71798ecc5166bd95cefbfa72c41e4153f718ec
Yago Ferro <son_cativo@hotmail.com>
2014-06-10 12:36:09 +0200

    pro credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,4 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, '306861178343-44gvfs26krqj4usqj4vkfkn438kh795e.apps.googleusercontent.com', 'Ki8ddU_DfXeRFDPetmX877nA'
   else  
     provider :google_oauth2, '306861178343-99tuadim60lb4akbtrsv2vgr61hr44hh.apps.googleusercontent.com', 'e1G-MdVWevlcPmBPiiJfKTmP'
+  end
 end
