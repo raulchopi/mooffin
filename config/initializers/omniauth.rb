5f8e76a0ec8879ac3b6ec159b067c931474f69e8
Yago Ferro <son_cativo@hotmail.com>
2014-06-10 12:33:54 +0200

    pro credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '526200534126885', '9ef00b7136fdd68277e7fb295e43855a'
   provider :twitter, '0RX7PDKKMkPwkkbGsKaNhQ', 'dKiCEMdMeNQwLZIIy8wUeMaDw7Mx8f3fjbzxwQSsnWI'
-  provider :google_oauth2, '306861178343-99tuadim60lb4akbtrsv2vgr61hr44hh.apps.googleusercontent.com', 'e1G-MdVWevlcPmBPiiJfKTmP'
+  if Rails.env.production?
+    provider :google_oauth2, '306861178343-44gvfs26krqj4usqj4vkfkn438kh795e.apps.googleusercontent.com', 'Ki8ddU_DfXeRFDPetmX877nA'
+  else  
+    provider :google_oauth2, '306861178343-99tuadim60lb4akbtrsv2vgr61hr44hh.apps.googleusercontent.com', 'e1G-MdVWevlcPmBPiiJfKTmP'
 end
