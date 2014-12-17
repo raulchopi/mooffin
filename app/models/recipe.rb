9905b5a9ef2d2e313b396bff21867f2e33a1919c
Raul Cacabelos Bote <raul.cacabelos@gmail.com>
2014-12-17 18:25:04 +0100

    created view with recipe totals and adapted query to propose recipes with brothers ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -22,13 +22,37 @@ class Recipe < ActiveRecord::Base
 
 	def self.find_proposals(idsIngredients)
 		logger.info('Dentro do find_proposals')
-		@proposals = Recipe.joins(:difficulty).joins(:links)
-			.joins("join importances on links.importance_id = importances.id")
-			.joins("left join likes on likes.recipe_id = recipes.id")
-			.joins("left join opinions on opinions.recipe_id = recipes.id")
-			.where(links: { ingredient_id: idsIngredients })
-			.select("recipes.*, difficulties.description AS dif_desc, sum(importances.weight) as weight, count(likes.id) as numLikes, count(opinions.id) as numOpinions")
-			.group("recipes.id, difficulties.id").order("weight desc")
+
+		@proposals = Recipe.find_by_sql ["SELECT recipes.*,
+			difficulties.description AS dif_desc,
+			sum(importances.weight) as weight,
+			rt.likes as num_likes,
+			rt.opinions as num_opinions
+			FROM recipes
+			INNER JOIN difficulties ON difficulties.id = recipes.difficulty_id
+			INNER JOIN links ON links.recipe_id = recipes.id
+			inner join ingredients ON ingredients.id = links.ingredient_id
+			join importances on links.importance_id = importances.id
+			join vw_recipe_totals rt on rt.recipe_id = recipes.id
+			WHERE ingredients.id IN (?)
+			GROUP BY recipes.id, difficulties.id, rt.likes, rt.opinions
+			UNION
+			SELECT recipes.*,
+			difficulties.description AS dif_desc,
+			sum(importances.weight*0.75) as weight,
+			rt.likes as num_likes,
+			rt.opinions as num_opinions
+			FROM recipes
+			INNER JOIN difficulties ON difficulties.id = recipes.difficulty_id
+			INNER JOIN links ON links.recipe_id = recipes.id
+			inner join ingredients ON ingredients.id = links.ingredient_id
+			join importances on links.importance_id = importances.id
+			join vw_recipe_totals rt on rt.recipe_id = recipes.id
+			WHERE ingredients.id NOT IN (?)
+			AND ingredients.parent_id IN (select parent_id from ingredients WHERE id in (?))
+			GROUP BY recipes.id, difficulties.id, rt.likes, rt.opinions
+			ORDER BY weight desc, num_likes desc, views_count desc",
+			idsIngredients, idsIngredients, idsIngredients]
 	end
 
 	def average_rate
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215153604) do
+ActiveRecord::Schema.define(version: 20141217171355) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id",    null: false
