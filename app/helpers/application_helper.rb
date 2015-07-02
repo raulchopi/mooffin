43824ea28852dee517c7b28b879140fb989f2f6e
Raúl Cacabelos Bote <raul.cacabelos@gmail.com>
2015-07-02 13:38:41 +0200

    Titulo de la página dinámico según receta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+	def title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? content_for(:title) + " | Mooffin" : "Mooffin - Buscador de recetas sociales por ingredientes"
+    end
+  end
+
 	def flash_class(level)
 		case level
 			when :notice then "alert alert-info"
@@ -8,5 +16,5 @@ module ApplicationHelper
 			when :alert then "alert alert-block"
 			else "alert #{level}"
 		end
-	end	
+	end
 end
