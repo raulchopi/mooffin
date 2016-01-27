ec5126bcf1dcbd3fdd56b89bf1f1a9ddd02e410e
Raúl Cacabelos Bote <raul.cacabelos@gmail.com>
2016-01-27 12:34:53 +0100

    Quito la protección por forgery para probar el api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-	protect_from_forgery with: :exception
+	protect_from_forgery with: :null_session
 
-	after_action  :set_csrf_cookie_for_ng
+	# after_action  :set_csrf_cookie_for_ng
 
-	def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
+	# def set_csrf_cookie_for_ng
+  #   cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  # end
 
 
 	protected
