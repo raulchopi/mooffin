7ad361d57048d99ab5d41a19c8300d3230191367
Raúl Cacabelos Bote <raul.cacabelos@gmail.com>
2016-01-28 10:01:40 +0100

    Vuelvo a dejar el filtro csrf como estaba, ya que había dejado de funcionar el 'current_user' de rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :null_session
 
-	# after_action  :set_csrf_cookie_for_ng
+	after_filter  :set_csrf_cookie_for_ng
 
-	# def set_csrf_cookie_for_ng
-  #   cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  # end
+	def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
 
 	protected
